ac72309c027212910fc0ee9e60ec332e1c95fca9
Matheus Luna de Oliveira Dantas <39143138+mattes10@users.noreply.github.com>
2020-03-08 17:46:25 -0300

    Add files via upload

--- Games_IMD/ball.gd
+++ Games_IMD/ball.gd
@@ -2,6 +2,7 @@ extends Sprite
 
 
 var mov=0;
+var movY=0
 
 func _process(delta):
 	
@@ -13,3 +14,11 @@ func _process(delta):
 	
 	if (position.x >= (get_viewport().size.x - 20) || position.x <= 20):
 		mov +=1;
+
+	if (movY % 2 == 0 || movY == 0):	
+		position.y += 200*delta
+	else:
+		position.y -= 200*delta
+	
+	if (position.y >= (get_viewport().size.y - 20) || position.y <= 20):
+		movY +=1;
